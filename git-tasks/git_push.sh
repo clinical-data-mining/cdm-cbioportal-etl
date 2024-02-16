1c737467906ecf20e9ad32d7b2bec487e27cc2b9
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-02-16 17:36:42 -0500

    Add commit_msg variable

--- git-tasks/git_push.sh
+++ git-tasks/git_push.sh
@@ -3,11 +3,10 @@ set -eE -v
 trap 'echo "Last command exited with status code of $?, exiting..."' ERR
 
 test -n "$GITHUB_REPO_PATH"
+test -n "$COMMIT_MSG"
 
-# GITHUB_REPO_PATH: github repository path
 cd "$GITHUB_REPO_PATH"
-
 git add *
 # `git commit` will exit 1 if there is nothing to commit, ignore the error in that case
-git commit -m "Latest MSK-CHORD Dataset" || true
+git commit -m "$COMMIT_MSG" || true
 git push origin
