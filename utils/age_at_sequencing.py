29243aed92ee91154915352177f44f6527b303da
Chris Fong <fongc2@mskcc.org>
2024-04-03 18:08:56 -0400

    Adding script to compute age at sequencing

---
+++ utils/age_at_sequencing.py
@@ -0,0 +1,96 @@
+"""
+age_at_sequencing.py
+
+Computes age at sequencing based on the available samples in the pathology report table (Darwin)
+Uses date of birth from demographics table
+
+Age at sequencing computed in days
+
+Data is saved to MinIO
+"""
+import argparse
+
+import pandas as pd
+
+from msk_cdm.minio import MinioAPI
+from msk_cdm.data_processing import mrn_zero_pad
+from msk_cdm.data_classes.legacy import CDMProcessingVariables as config_cdm
+
+def compute_age_at_sequencing(
+        *,
+        minio_env,
+        fname_demo,
+        fname_samples,
+        fname_save_age_at_seq
+):
+    """
+
+    :param minio_env: MinIO environment filename
+    :param fname_demo: object path to demographics table
+    :param fname_samples: object path to the pathology report table
+    :param fname_save_age_at_seq: object path to where age at sequencing is saved
+    :return: df_f: dataframe with age at sequencing
+    """
+
+    # Load data
+    ## Create Minio object
+    obj_minio = MinioAPI(fname_minio_env=minio_env)
+
+    ## Load demographics for date of birth
+    col_keep = ['MRN', 'PT_BIRTH_DTE']
+    obj = obj_minio.load_obj(path_object=fname_demo)
+    df_demo = pd.read_csv(obj, sep='\t', usecols=col_keep)
+    df_demo = mrn_zero_pad(df=df_demo, col_mrn='MRN')
+    df_demo['PT_BIRTH_DTE'] = pd.to_datetime(df_demo['PT_BIRTH_DTE'])
+
+    ## Load pathology report table
+    col_keep = ['MRN', 'DTE_PATH_PROCEDURE', 'DMP_ID', 'SAMPLE_ID']
+    obj = obj_minio.load_obj(path_object=fname_samples)
+    df_path = pd.read_csv(obj, sep='\t', usecols=col_keep, low_memory=False)
+    df_path = mrn_zero_pad(df=df_path, col_mrn='MRN')
+
+    # Clean and Combine data
+    df_path_clean = df_path[df_path['SAMPLE_ID'].notnull()].copy()
+    df_path_clean['DTE_PATH_PROCEDURE'] = pd.to_datetime(df_path_clean['DTE_PATH_PROCEDURE'])
+
+    ## Merge dataframes
+    df_f = df_path_clean.merge(right=df_demo, how='left', on=['MRN'])
+
+    ## Compute age at sequencing
+    df_f['AGE_AT_SEQUENCING_DAYS'] = (df_f['DTE_PATH_PROCEDURE'] - df_f['PT_BIRTH_DTE']).dt.days
+
+    ## Drop columns that contain PHI
+    cols_phi = ['MRN', 'DTE_PATH_PROCEDURE', 'PT_BIRTH_DTE']
+    df_f = df_f.drop(columns=cols_phi)
+
+    # Save dataframe
+    obj_minio.save_obj(
+        df=df_f,
+        path_object=fname_save_age_at_seq,
+        sep='\t'
+    )
+
+    return df_f
+
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser(description="cBioPortal timeline file for cancer progression predictions")
+    # parser.add_argument(
+    #     "--fname_log",
+    #     action="store",
+    #     dest="fname_log",
+    #     help="Log to indicate data is complete and can be pushed to datahub."
+    # )
+    # args = parser.parse_args()
+
+    ENV_MINIO = config_cdm.minio_env
+    fname_demo = config_cdm.fname_demo
+    fname_samples = config_cdm.fname_path_clean
+    fname_save_age_at_seq = 'cbioportal/age_at_sequencing.tsv'
+
+    compute_age_at_sequencing(
+        minio_env=ENV_MINIO,
+        fname_demo=fname_demo,
+        fname_samples=fname_samples,
+        fname_save_age_at_seq=fname_save_age_at_seq
+    )
