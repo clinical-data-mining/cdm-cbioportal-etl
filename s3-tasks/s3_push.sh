ae7326c5a45fb3842d9f613237d779b23ff033fa
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-04-26 13:37:54 -0400

    Update path to authenticate script

--- s3-tasks/s3_push.sh
+++ s3-tasks/s3_push.sh
@@ -1,5 +1,4 @@
-# TODO iron out path?
-/mind_data/cdm_repos/cdm-cbioportal-etl/s3-tasks/authenticate_service_account.sh eks
+/gpfs/mindphidata/cdm_repos/github/cdm-cbioportal-etl/s3-tasks/authenticate_service_account.sh private
 aws s3 sync $INPUT_DIR s3://cdm-deliverable/$OUTPUT_DIR --profile saml
 if [ $? -ne 0 ] ; then
   echo "`date`: Failed to upload CDM annotated data to S3 bucket, exiting..."
