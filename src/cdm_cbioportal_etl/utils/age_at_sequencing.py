4fe78178111761c11e87f4b03fe5645dbdd19e36
Chris Fong <fongc2@mskcc.org>
2024-04-26 15:57:01 -0400

    Updating age at sequencing processing

--- src/cdm_cbioportal_etl/utils/age_at_sequencing.py
+++ src/cdm_cbioportal_etl/utils/age_at_sequencing.py
@@ -57,10 +57,12 @@ def compute_age_at_sequencing(
 
     ## Load anchor dates
     df_archor_dates = get_anchor_dates()
-    df_sample_ids_used = df_archor_dates[['SAMPLE_ID']].copy()
+    list_sample_ids_used = list(set(df_archor_dates['DMP_ID']))
 
     # Clean and Combine data
-    df_path_clean = df_path[df_path['SAMPLE_ID'].notnull()].copy()
+    df_path_clean = df_path[df_path['SAMPLE_ID'].notnull() & df_path['DMP_ID'].isin(list_sample_ids_used)].copy()
+    df_path_clean['DMP_ID_DERIVED'] = df_path_clean['SAMPLE_ID'].apply(lambda x: x[:9])
+    df_path_clean = df_path_clean[df_path_clean['DMP_ID_DERIVED'] == df_path_clean['DMP_ID']].copy()
     df_path_clean['DTE_PATH_PROCEDURE'] = pd.to_datetime(df_path_clean['DTE_PATH_PROCEDURE'])
 
     ## Merge dataframes
@@ -74,20 +76,21 @@ def compute_age_at_sequencing(
 
     df_f['AGE_AT_SEQUENCING_YEARS_PHI'] = (df_f['AGE_AT_SEQUENCING_DAYS_PHI']/AGE_CONVERSION_FACTOR)
     df_f['AGE_AT_SEQUENCING_YEARS_WITH_OS_INT_PHI'] = ((df_f['AGE_AT_SEQUENCING_DAYS_PHI'] + df_f['OS_INT'])/AGE_CONVERSION_FACTOR)
-    df_f = convert_to_int(df=df_f, list_cols=['AGE_AT_SEQUENCING_YEARS_PHI', 'AGE_AT_SEQUENCING_YEARS_WITH_OS_INT_PHI'])
+
+    df_f['AGE_AT_SEQUENCING_YEARS_PHI'] = df_f['AGE_AT_SEQUENCING_YEARS_PHI'].astype(int)
+    df_f['AGE_AT_SEQUENCING_YEARS_WITH_OS_INT_PHI'] = df_f['AGE_AT_SEQUENCING_YEARS_WITH_OS_INT_PHI'].astype(int)
 
     ## Deidentify age
     log_under18 = df_f['AGE_AT_SEQUENCING_YEARS_PHI'] < 18
-    log_over89 = df_f['AGE_AT_SEQUENCING_YEARS_WITH_OS_INT_PHI'] > 89
+    log_over89 = (df_f['AGE_AT_SEQUENCING_YEARS_WITH_OS_INT_PHI'] > 89) | (df_f['AGE_AT_SEQUENCING_YEARS_PHI'] > 89)
 
     df_f['AGE_AT_SEQUENCING_YEARS'] = df_f['AGE_AT_SEQUENCING_YEARS_PHI'].astype(str)
     df_f.loc[log_under18, 'AGE_AT_SEQUENCING_YEARS'] = '<18'
-    df_f.loc[log_over89, 'AGE_AT_SEQUENCING_YEARS'] = '>' + df_f.loc[log_over89, 'AGE_AT_SEQUENCING_YEARS_PHI']
+    df_f.loc[log_over89, 'AGE_AT_SEQUENCING_YEARS'] = '>' + df_f.loc[log_over89, 'AGE_AT_SEQUENCING_YEARS']
 
     ## Drop columns that contain PHI
     cols_keep = ['DMP_ID', 'SAMPLE_ID', 'AGE_AT_SEQUENCING_YEARS']
     # df_f = df_f[cols_keep]
-    df_f = df_f[df_f['SAMPLE_ID'].isin(df_sample_ids_used['SAMPLE_ID'])].copy()
 
     # Save dataframe
     obj_minio.save_obj(
