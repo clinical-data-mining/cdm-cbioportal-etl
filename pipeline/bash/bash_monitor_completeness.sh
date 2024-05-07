8ec2420be9bc9832f3360733d48fc53cd9f86fc2
Chris Fong <fongc2@mskcc.org>
2024-05-07 17:42:59 -0400

    Adding script to copy to minio and monitor for completeness

---
+++ pipeline/bash/bash_monitor_completeness.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Define which variables to use from msk_cdm.data_classes.<class> library
+#VAR_SCRIPT="config_cbio_etl.script_create_summary_templates"
+
+
+set -e
+
+# Activate virtual env
+source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
+conda activate conda-env-cdm
+
+# Get variables
+#SCRIPT=$(python -c "from msk_cdm.data_classes.legacy import CDMProcessingVariablesCbioportal as config_cbio_etl; print (${VAR_SCRIPT})")
+SCRIPT="/gpfs/mindphidata/fongc2/github/cdm-cbioportal-etl/pipeline/monitoring/monitoring_completeness.py"
+
+# Run script
+python $SCRIPT \
+  --fname_log="$FNAME_LOG"
+
