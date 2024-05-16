2c15112273a7b2bd3e4742773a260e9576ecbfdd
Chris Fong <fongc2@mskcc.org>
2024-05-16 18:12:28 -0400

    Adding scripts to run testing portal pipeline

--- pipeline/summary/wrapper_cbioportal_summary_creator_testing_study.py
+++ pipeline/summary/wrapper_cbioportal_summary_creator_testing_study.py
@@ -47,54 +47,54 @@ def create_cbioportal_summary(
     fname_meta_table,
     fname_meta_project
 ):
-    list_ = [
-        fname_minio_env,
-        patient_or_sample,
-        fname_manifest,
-        fname_current_summary,
-        fname_new_summary,
-        production_or_test,
-        fname_save_var_summary,
-        fname_save_header_summary,
-        path_minio_summary_intermediate,
-        fname_meta_data,
-        fname_meta_table,
-        fname_meta_project
-    ]
+    # list_ = [
+    #     fname_minio_env,
+    #     patient_or_sample,
+    #     fname_manifest,
+    #     fname_current_summary,
+    #     fname_new_summary,
+    #     production_or_test,
+    #     fname_save_var_summary,
+    #     fname_save_header_summary,
+    #     path_minio_summary_intermediate,
+    #     fname_meta_data,
+    #     fname_meta_table,
+    #     fname_meta_project
+    # ]
 
-    print(list_)
-    # obj_format_cbio = RedcapToCbioportalFormat(
-    #     fname_minio_env=fname_minio_env,
-    #     path_minio_summary_intermediate=path_minio_summary_intermediate,
-    #     fname_metadata=fname_meta_data,
-    #     fname_metaproject=fname_meta_project,
-    #     fname_metatables=fname_meta_table
-    #
-    # )
-    #
-    # ## Create individual summary and header files, with a manifest file summarizing the outputs
-    # obj_format_cbio.create_summaries_and_headers(
-    #     patient_or_sample=patient_or_sample,
-    #     fname_manifest=fname_manifest,
-    #     fname_template=fname_current_summary,
-    #     production_or_test=production_or_test
-    # )
-    #
-    # ## Merge summaries and headers
-    # obj_p_combiner = cbioportalSummaryFileCombiner(
-    #     fname_minio_env=fname_minio_env,
-    #     fname_manifest=fname_manifest,
-    #     fname_current_summary=fname_current_summary,
-    #     patient_or_sample=patient_or_sample,
-    #     production_or_test=production_or_test,
-    #     fname_save_var_summary=fname_save_var_summary,
-    #     fname_save_header_summary=fname_save_header_summary
-    #
-    # )
-    #
-    # # Save the merged summaries to file
-    # obj_p_combiner.save_update(fname=fname_new_summary)
-    # df_cbio_summary = obj_p_combiner.return_final()
+    # print(list_)
+    obj_format_cbio = RedcapToCbioportalFormat(
+        fname_minio_env=fname_minio_env,
+        path_minio_summary_intermediate=path_minio_summary_intermediate,
+        fname_metadata=fname_meta_data,
+        fname_metaproject=fname_meta_project,
+        fname_metatables=fname_meta_table
+
+    )
+
+    ## Create individual summary and header files, with a manifest file summarizing the outputs
+    obj_format_cbio.create_summaries_and_headers(
+        patient_or_sample=patient_or_sample,
+        fname_manifest=fname_manifest,
+        fname_template=fname_current_summary,
+        production_or_test=production_or_test
+    )
+
+    ## Merge summaries and headers
+    obj_p_combiner = cbioportalSummaryFileCombiner(
+        fname_minio_env=fname_minio_env,
+        fname_manifest=fname_manifest,
+        fname_current_summary=fname_current_summary,
+        patient_or_sample=patient_or_sample,
+        production_or_test=production_or_test,
+        fname_save_var_summary=fname_save_var_summary,
+        fname_save_header_summary=fname_save_header_summary
+
+    )
+
+    # Save the merged summaries to file
+    obj_p_combiner.save_update(fname=fname_new_summary)
+    df_cbio_summary = obj_p_combiner.return_final()
 
 
 def main():
@@ -145,7 +145,7 @@ def main():
         "--production_or_test",
         action="store",
         dest="production_or_test",
-        default='production',
+        default='test',
         help="For logic to decide if production portal or testing portal will be updated.",
     )
     parser.add_argument(
