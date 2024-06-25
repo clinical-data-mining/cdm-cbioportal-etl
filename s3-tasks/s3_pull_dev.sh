9853a615e0c9ac79a7e8aee33856b22637cf35e4
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-06-25 16:50:19 -0400

    Change s3 pull script to use dev auth script

--- s3-tasks/s3_pull_dev.sh
+++ s3-tasks/s3_pull_dev.sh
@@ -4,5 +4,5 @@ trap 'echo "Last command exited with status code of $?, exiting..."' ERR
 
 echo $PATH
 
-/gpfs/mindphidata/cdm_repos/github/cdm-cbioportal-etl/s3-tasks/authenticate_service_account.sh private
+/gpfs/mindphidata/cdm_repos/github/cdm-cbioportal-etl/s3-tasks/authenticate_service_account_dev.sh private
 aws s3 cp s3://cdm-deliverable/data_clinical_sample.txt {{ params.output_dir }} --profile saml
