c4c99c2cbd175cdc7764139136ac9c35d105af9e
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-06-25 17:07:16 -0400

    take output dir as command line arg

--- s3-tasks/s3_pull_dev.sh
+++ s3-tasks/s3_pull_dev.sh
@@ -3,6 +3,7 @@ set -eE -v
 trap 'echo "Last command exited with status code of $?, exiting..."' ERR
 
 echo $PATH
+OUTPUT_DIR=$1
 
 /gpfs/mindphidata/cdm_repos/github/cdm-cbioportal-etl/s3-tasks/authenticate_service_account_dev.sh private
-aws s3 cp s3://cdm-deliverable/data_clinical_sample.txt {{ params.output_dir }} --profile saml
+aws s3 cp s3://cdm-deliverable/data_clinical_sample.txt $OUTPUT_DIR --profile saml
