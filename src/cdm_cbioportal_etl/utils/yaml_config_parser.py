d77852aec9aa5f8c2b990b089271fc7100a42440
fongc2 <fongc2@mskcc.org>
2024-09-13 12:52:56 -0400

    Updating yaml parser to include other variables

--- src/cdm_cbioportal_etl/utils/yaml_config_parser.py
+++ src/cdm_cbioportal_etl/utils/yaml_config_parser.py
@@ -16,11 +16,46 @@ class YamlParser(object):
         Args:
             config_path (str): Path to the YAML configuration file.
         """
+        # Codebook variables
+        self._df_codebook_metadata = None
+        self._df_codebook_table = None
+        self._df_codebook_project = None
+
         # Load the YAML configuration file
         with open(fname_yaml_config, 'r') as yaml_file:
             config = yaml.safe_load(yaml_file)
-
         self._config = config
+        self._load_codebook()
+
+    def _load_codebook(self):
+        # Load the YAML configuration file
+        config = self._config
+
+        # Codebook path
+        path_codebook = config.get('codebook', {}).get('path')
+
+        # Load metadata sheet
+        f = config.get('codebook', {}).get('fname_metadata')
+        filename = os.path.join(path_codebook, f)
+        # Load the JSON mapping file
+        with open(filename, 'r') as fname_codebook_metadata:
+            self._df_codebook_metadata = pd.read_csv(fname_codebook_metadata)
+
+        # Load table sheet
+        f = config.get('codebook', {}).get('fname_tables')
+        filename = os.path.join(path_codebook, f)
+        # Load the JSON mapping file
+        with open(filename, 'r') as fname_codebook_table:
+            self._df_codebook_table = pd.read_csv(fname_codebook_table)
+
+        # Load project sheet
+        f = config.get('codebook', {}).get('fname_project')
+        filename = os.path.join(path_codebook, f)
+        # Load the JSON mapping file
+        with open(filename, 'r') as fname_codebook_project:
+            self._df_codebook_project = pd.read_csv(fname_codebook_project)
+
+        return None
 
 
     def return_template_info(self) -> dict:
@@ -70,14 +105,14 @@ class YamlParser(object):
         return dict_deid_filenames_minio
 
     def return_dict_copy_to_minio(self):
-        dict_deid_filenames_deid_minio = self.return_filenames_deid_minio()
+        dict_deid_filenames_minio = self.return_filenames_deid_minio()
         dict_deid_filenames_deid_datahub = self.return_filenames_deid_datahub()
 
         # Combine the values from both dictionaries using the same keys
         combined_dict = {
-            dict_deid_filenames_deid_datahub[key]: dict_deid_filenames_deid_minio[key]
-            for key in dict_deid_filenames_deid_datahub
-            if key in dict_deid_filenames_deid_minio
+            dict_deid_filenames_datahub[key]: dict_deid_filenames_minio[key]
+            for key in dict_deid_filenames_datahub
+            if key in dict_deid_filenames_minio
         }
 
         return combined_dict
@@ -95,11 +130,7 @@ class YamlParser(object):
             dict: A dictionary with the mapped filenames.
         """
         config = self._config
-
-        mapping_path = config.get('codebook', 'fname_tables')
-        # Load the JSON mapping file
-        with open(mapping_path, 'r') as fname_codebook_table:
-            mapping = pd.read_csv(fname_codebook_table)
+        mapping = self._df_codebook_table
 
         # Access the filenames in the YAML file
         template_files = config.get('template_files', {})
