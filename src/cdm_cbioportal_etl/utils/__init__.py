5b02dd354a8709cc4d821a06b0fb3ba47e1ef433
fongc2 <fongc2@mskcc.org>
2024-09-16 11:21:15 -0400

    bug fix for function name

--- src/cdm_cbioportal_etl/utils/__init__.py
+++ src/cdm_cbioportal_etl/utils/__init__.py
@@ -1,11 +1,11 @@
 from .get_anchor_dates import get_anchor_dates
 from .age_at_sequencing import compute_age_at_sequencing
 from .constants import constants
-from .yaml_config_parser import YamlParser as yaml_parser
+from .yaml_config_parser import YamlParser as yaml_config_parser
 
 __all__ = [
     "get_anchor_dates",
     "compute_age_at_sequencing",
     "constants",
-    "yaml_parser"
+    "yaml_config_parser"
 ]
