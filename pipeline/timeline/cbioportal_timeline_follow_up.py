878ee19b243063d6052121b719b3eb28594bbf7d
fongc2 <fongc2@mskcc.org>
2024-09-16 17:21:48 -0400

    - Adding scripts for f/u timeline

--- pipeline/timeline/cbioportal_timeline_follow_up.py
+++ pipeline/timeline/cbioportal_timeline_follow_up.py
@@ -1,15 +1,18 @@
-import os
-import sys
+import argparse
+# import os
+# import sys
 
 import pandas as pd
 
-sys.path.insert(0,  os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
-from variables import (
-    ENV_MINIO,
-    FNAME_DEMO,
-    FNAME_TIMELINE_FU
-)
+# sys.path.insert(0,  os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+# from variables import (
+#     ENV_MINIO,
+#     FNAME_DEMO,
+#     FNAME_TIMELINE_FU
+# )
+from cdm_cbioportal_etl.utils import yaml_config_parser
 from msk_cdm.minio import MinioAPI
+from msk_cdm.data_classes.legacy import CDMProcessingVariables as cdm_files
 
 
 ## Constants
@@ -23,39 +26,69 @@ rep_dict = {
     }
 }
 
-## Create timeline file for follow-up
-### Load data
-print('Loading %s' % FNAME_DEMO)
-obj_minio = MinioAPI(fname_minio_env=ENV_MINIO)
-obj = obj_minio.load_obj(path_object=FNAME_DEMO)
-df_demo = pd.read_csv(obj, sep='\t', usecols=col_keep)
+def cbioportal_timeline_follow_up(
+        yaml_config,
+        fname_demo,
+        fname_save
+):
+    obj_yaml = yaml_config_parser(fname_yaml_config=yaml_config)
+    fname_minio_env = obj_yaml.return_credential_filename()
 
-print('Creating timeline')
-df_os_ = pd.melt(
-    frame=df_demo, 
-    id_vars='MRN', 
-    value_vars=[
-        'MRN_CREATE_DTE', 
-        'PLA_LAST_CONTACT_DTE', 
-        'PT_DEATH_DTE'
-    ], 
-    value_name='START_DATE',
-    var_name='SOURCE'
-)
-        
-    
-df_os_ = df_os_.assign(STOP_DATE='')
-df_os_ = df_os_.assign(EVENT_TYPE='Diagnosis')
-df_os_ = df_os_.assign(SUBTYPE='Follow-Up')
 
-df_os_ = df_os_[df_os_['START_DATE'].notnull()].copy()
-df_os_ = df_os_[col_order]
-df_os_ = df_os_.replace(rep_dict)
+    ## Create timeline file for follow-up
+    ### Load data
+    print('Loading %s' % fname_demo)
+    obj_minio = MinioAPI(fname_minio_env=fname_minio_env)
+    obj = obj_minio.load_obj(path_object=fname_demo)
+    df_demo = pd.read_csv(obj, sep='\t', usecols=col_keep)
 
-### Save data
-print('Saving %s to MinIO' % FNAME_TIMELINE_FU)
-obj_minio.save_obj(
-    df=df_os_,
-    path_object=FNAME_TIMELINE_FU,
-    sep='\t'
-)
+    print('Creating timeline')
+    df_os_ = pd.melt(
+        frame=df_demo,
+        id_vars='MRN',
+        value_vars=[
+            'MRN_CREATE_DTE',
+            'PLA_LAST_CONTACT_DTE',
+            'PT_DEATH_DTE'
+        ],
+        value_name='START_DATE',
+        var_name='SOURCE'
+    )
+
+
+    df_os_ = df_os_.assign(STOP_DATE='')
+    df_os_ = df_os_.assign(EVENT_TYPE='Diagnosis')
+    df_os_ = df_os_.assign(SUBTYPE='Follow-Up')
+
+    df_os_ = df_os_[df_os_['START_DATE'].notnull()].copy()
+    df_os_ = df_os_[col_order]
+    df_os_ = df_os_.replace(rep_dict)
+
+    ### Save data
+    print('Saving %s to MinIO' % fname_save)
+    obj_minio.save_obj(
+        df=df_os_,
+        path_object=fname_save,
+        sep='\t'
+    )
+
+    return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script for creating a template of patient and sample IDs for summary and timeline files")
+    parser.add_argument(
+        "--config_yaml",
+        action="store",
+        dest="config_yaml",
+        help="Yaml file containing run parameters and necessary file locations.",
+    )
+    args = parser.parse_args()
+
+    fname_demo = cdm_files.fname_demo
+    fname_fu_save = cdm_files.fname_timeline_fu
+
+    cbioportal_timeline_follow_up(
+        yaml_config=args.config_yaml,
+        fname_demo=fname_demo,
+        fname_save=fname_fu_save
+    )
