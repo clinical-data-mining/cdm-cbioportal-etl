b9d5c71ce34a0171e7d6a1c1588d94c8ba2dadac
fongc2 <fongc2@mskcc.org>
2024-09-17 17:25:17 -0400

    - Adding scripts for test study processing

--- pipeline/bash/bash_copy_to_minio_testing_study.sh
+++ pipeline/bash/bash_copy_to_minio_testing_study.sh
@@ -2,6 +2,7 @@
 
 REPO_LOCATION="/gpfs/mindphidata/fongc2/github/"
 
+
 set -e
 
 # Activate virtual env
@@ -9,11 +10,9 @@ source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
 conda activate conda-env-cdm-fongc2
 
 # Get variables
-#SCRIPT=$(python -c "from msk_cdm.data_classes.legacy import CDMProcessingVariablesCbioportal as config_cbio_etl; print (${VAR_SCRIPT})")
-SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/summary/wrapper_cbioportal_summary_creator.py"
+SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/utils/wrapper_cbioportal_copy_to_minio.py"
 YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/etl_config_all_impact_testing_study.yml"
 
 # Run script
 python $SCRIPT --config_yaml=$YAML_CONFIG
-
 
