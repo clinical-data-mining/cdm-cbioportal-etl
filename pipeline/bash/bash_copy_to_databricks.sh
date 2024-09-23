050c880dd2ff6242fb7f71e061e8646ea0c87881
fongc2 <fongc2@mskcc.org>
2024-09-23 14:37:51 -0400

    Adding feature to production

--- pipeline/bash/bash_copy_to_databricks.sh
+++ pipeline/bash/bash_copy_to_databricks.sh
@@ -11,7 +11,7 @@ conda activate conda-env-cdm-fongc2
 
 # Get variables
 SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/utils/wrapper_cbioportal_copy_to_databricks.py"
-YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/etl_config_all_impact_testing_study.yml"
+YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/etl_config_all_impact.yml"
 
 # Run script
 python $SCRIPT --config_yaml=$YAML_CONFIG
