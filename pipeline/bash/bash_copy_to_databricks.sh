e4db2282bdee76e73cf916bb1df8e439473ca2e3
fongc2 <fongc2@mskcc.org>
2024-09-25 15:51:25 -0400

    Adding script to copy minio files into databricks

--- pipeline/bash/bash_copy_to_databricks.sh
+++ pipeline/bash/bash_copy_to_databricks.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-REPO_LOCATION="/gpfs/mindphidata/fongc2/github/"
+REPO_LOCATION="/gpfs/mindphidata/cdm_repos/github/"
 
 
 set -e
 
 # Activate virtual env
 source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
-conda activate conda-env-cdm-fongc2
+conda activate conda-env-cdm
 
 # Get variables
 SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/utils/wrapper_cbioportal_copy_to_databricks.py"
-YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/etl_config_all_impact.yml"
+YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/config_databricks_copy.yml"
 
 # Run script
 python $SCRIPT --config_yaml=$YAML_CONFIG
