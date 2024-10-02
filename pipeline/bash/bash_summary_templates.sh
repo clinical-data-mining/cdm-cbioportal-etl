3d3eba2da5cf11f17180eadcff353294dfaf74fe
fongc2 <fongc2@mskcc.org>
2024-10-02 12:46:40 -0400

    fix on file name

--- pipeline/bash/bash_summary_templates.sh
+++ pipeline/bash/bash_summary_templates.sh
@@ -8,6 +8,8 @@ set -e
 source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
 conda activate conda-env-cdm
 
+echo $ REPO_LOCATION
+
 # Get variables
 SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/utils/generate_cbioportal_template.py"
 #YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/etl_config_all_impact.yml"
