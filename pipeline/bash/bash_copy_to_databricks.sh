1e2ecb2d2da46c5d576cd76b7614041fa9422aac
fongc2 <fongc2@mskcc.org>
2024-10-03 17:51:33 -0400

    Updating databricks copying script to run with airflow

--- pipeline/bash/bash_copy_to_databricks.sh
+++ pipeline/bash/bash_copy_to_databricks.sh
@@ -10,8 +10,8 @@ source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
 conda activate conda-env-cdm
 
 # Get variables
-SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/utils/wrapper_cbioportal_copy_to_databricks.py"
-YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/config_databricks_copy.yml"
+SCRIPT="${REPO_LOCATION}pipeline/utils/wrapper_cbioportal_copy_to_databricks.py"
+YAML_CONFIG="${REPO_LOCATION}config/config_databricks_copy.yml"
 
 # Run script
 python $SCRIPT --config_yaml=$YAML_CONFIG
