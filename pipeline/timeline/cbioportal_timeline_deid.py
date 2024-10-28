a42bac71cdaafe50ce9b8a4cde8d2723f8cb2de8
fongc2 <fongc2@mskcc.org>
2024-10-28 11:27:24 -0400

    updating timeline processing by filtering by list of patient ids

--- pipeline/timeline/cbioportal_timeline_deid.py
+++ pipeline/timeline/cbioportal_timeline_deid.py
@@ -1,5 +1,7 @@
 import argparse
 
+import pandas as pd
+
 from cdm_cbioportal_etl.utils import cbioportal_update_config
 from cdm_cbioportal_etl.timeline import cbioportal_deid_timeline_files
 
@@ -20,7 +22,10 @@ def main():
     ENV_MINIO = obj_yaml.return_credential_filename()
     production_or_test = obj_yaml.return_production_or_test_indicator()
 
-
+    # Get list of sample and patient IDs used for cbioportal ETL
+    fname_sample = obj_yaml.return_sample_list_filename()
+    df_samples_used = pd.read_csv(fname_sample, sep='\t')
+    list_dmp_ids = list(df_samples_used['PATIENT_ID'].drop_duplicates())
 
     if production_or_test == 'production':
         dict_files_timeline = DICT_FILES_TIMELINE
@@ -32,8 +37,10 @@ def main():
 
     _ = cbioportal_deid_timeline_files(
         fname_minio_env=ENV_MINIO,
-        dict_files_timeline=dict_files_timeline
+        dict_files_timeline=dict_files_timeline,
+        list_dmp_ids=list_dmp_ids
     )
+
 
 
 if __name__ == '__main__':
