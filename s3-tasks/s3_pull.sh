d5032b9e4316afc17ac778c81819b4ae5dd56c53
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-10-31 17:36:38 -0400

    update s3 push/pull scripts

--- s3-tasks/s3_pull.sh
+++ s3-tasks/s3_pull.sh
@@ -2,7 +2,15 @@
 set -eE -v
 trap 'echo "Last command exited with status code of $?, exiting..."' ERR
 
+test -n "$SCRIPTS_PATH"
+test -n "$CLUSTER_NAME"
+test -n "$BUCKET_NAME"
+test -n "$SAMPLE_FILEPATH"
 test -n "$OUTPUT_DIR"
 
-/gpfs/mindphidata/cdm_repos/github/cdm-cbioportal-etl/s3-tasks/authenticate_service_account.sh private
-aws s3 cp s3://cdm-deliverable/data_clinical_sample.txt $OUTPUT_DIR --profile saml
+# Create directory if it doesn't exist
+mkdir -p $OUTPUT_DIR
+
+# Authenticate and download clinical sample file from S3
+$SCRIPTS_PATH/authenticate_service_account.sh $CLUSTER_NAME
+aws s3 cp s3://$BUCKET_NAME/$SAMPLE_FILEPATH $OUTPUT_DIR --profile saml
