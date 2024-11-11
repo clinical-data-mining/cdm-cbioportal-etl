a005db8ba9be98a5758a0c0274cbd532dabfffff
fongc2 <fongc2@mskcc.org>
2024-11-11 11:21:29 -0500

    Changing location of bash files

--- pipeline/bash/bash_summary_creator.sh
+++ pipeline/bash/bash_summary_creator.sh
@@ -8,9 +8,13 @@ set -e
 source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
 conda activate conda-env-cdm
 
+MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+cd $MY_PATH
+cd ../summary
+
 # Get variables
 #SCRIPT=$(python -c "from msk_cdm.data_classes.legacy import CDMProcessingVariablesCbioportal as config_cbio_etl; print (${VAR_SCRIPT})")
-SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/summary/wrapper_cbioportal_summary_creator.py"
+SCRIPT=wrapper_cbioportal_summary_creator.py
 #YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/etl_config_mskimpact.yml"
 
 # Run script
