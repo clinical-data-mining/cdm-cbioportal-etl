d679bbda30c6422d730f210a19e1d33fcd95cbad
fongc2 <fongc2@mskcc.org>
2024-11-13 10:13:28 -0500

    Changing timeline generation for testing

--- pipeline/bash/bash_timeline_deidentify_testing_study.sh
+++ pipeline/bash/bash_timeline_deidentify_testing_study.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-REPO_LOCATION="/gpfs/mindphidata/fongc2/github/"
-
 set -e
+
+YAML_CONFIG="/gpfs/mindphidata/cdm_repos/github/cdm-cbioportal-etl/config/etl_config_all_impact_testing_study.yml"
 
 # Activate virtual env
 source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
 conda activate conda-env-cdm
 
+MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+cd $MY_PATH
+cd ../timeline
+
 # Get variables
 #SCRIPT=$(python -c "from msk_cdm.data_classes.legacy import CDMProcessingVariablesCbioportal as config_cbio_etl; print (${VAR_SCRIPT})")
-SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/timeline/cbioportal_timeline_deid.py"
-YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/etl_config_all_impact_testing_study.yml"
+SCRIPT=cbioportal_timeline_deid.py
 
 # Run script
 python $SCRIPT --config_yaml=$YAML_CONFIG
