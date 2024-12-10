67b6fa2e65bbd67bd547bf830ec37fd14bc99aaa
fongcj <fongc2@mskcc.org>
2024-12-10 10:22:17 -0800

    Merge pull request #124
    * Adding scripts for computing date of seq
    
    * Changing location of date of seq bash
    
    * Adding function to package
    
    * Fix on package

---
+++ pipeline/bash/bash_date_of_sequencing.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+REPO_LOCATION="/gpfs/mindphidata/fongc2/github/"
+FNAME_SAVE="cbioportal/seq_date.txt"
+
+set -e
+
+# Activate virtual env
+source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
+conda activate conda-env-cdm-fongc2
+
+# Get variables
+SCRIPT="${REPO_LOCATION}cdm-cbioportal-etl/pipeline/utils/generate_date_of_sequencing.py"
+YAML_CONFIG="${REPO_LOCATION}cdm-cbioportal-etl/config/etl_config_all_impact.yml"
+
+# Run script
+python $SCRIPT --config_yaml=$YAML_CONFIG --fname_save_date_of_seq=$FNAME_SAVE
+
