a9424cdf5967d43f5fa54897126e4434f894575f
fongc2 <fongc2@mskcc.org>
2025-04-18 19:09:36 -0400

    Fix for landing location of summary table

--- src/cdm_cbioportal_etl/utils/get_anchor_dates.py
+++ src/cdm_cbioportal_etl/utils/get_anchor_dates.py
@@ -29,31 +29,23 @@ def get_anchor_dates():
         df_path['DTE_TUMOR_SEQUENCING'],
         errors='coerce'
     )
-    print(df_path.head())
+
     logic_filt1 = df_path['SAMPLE_ID'].notnull()
     logic_filt2 = df_path['SAMPLE_ID'].str.contains('T')
     logic_filt3 = df_path['DTE_TUMOR_SEQUENCING'].notnull()
     logic_filt = logic_filt1 & logic_filt2 & logic_filt3
 
     df_path_filt = df_path[logic_filt].copy()
-    print(df_path_filt.head())
-
     df_path_filt['DMP_ID_DERIVED'] = df_path_filt['SAMPLE_ID'].apply(lambda x: x[:9])
-
-    print(df_path_filt.head())
 
     # Remove cases where DMP_ID does not match DMP_ID Derived
     df_path_sample_id_error = df_path_filt[df_path_filt['DMP_ID_DERIVED'] != df_path_filt['DMP_ID']]
-    print(df_path_sample_id_error.head())
 
     df_path_filt_clean1 = df_path_filt[df_path_filt['DMP_ID_DERIVED'] == df_path_filt['DMP_ID']]
     df_path_filt_clean1 = df_path_filt_clean1.sort_values(by=['MRN', 'DTE_TUMOR_SEQUENCING'])
 
-    print(f"df_path_filt_clean1: {df_path_filt_clean1.head()}")
-
     df_path_g = df_path_filt_clean1.groupby(['MRN', 'DMP_ID'])['DTE_TUMOR_SEQUENCING'].min().reset_index()
 
-    print(f"df_path_g: {df_path_g.head()}")
 
     # Remove any MRN or DMP-ID in df_path_g_error
     filt_rmv_patients = df_path_g['DMP_ID'].isin(df_path_sample_id_error['DMP_ID']) | \
@@ -62,10 +54,8 @@ def get_anchor_dates():
 
     df_path_g_f = df_path_g[~filt_rmv_patients]
 
-    print(f"df_path_g_f: {df_path_g_f.head()}")
-
     print('Error in mapping summary:')
-    print(df_path_sample_id_error.shape)
+    print(df_path_sample_id_error)
     
     print('Done!')
     
