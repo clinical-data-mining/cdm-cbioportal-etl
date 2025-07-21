f0087936a86393a10bf558a3b7ca56203cb81da0
fongc2 <fongc2@mskcc.org>
2025-07-21 19:08:53 -0400

    Updating anchor date processing for removing MRNs and DMP-IDs that have a mismatch

--- src/cdm_cbioportal_etl/utils/get_anchor_dates.py
+++ src/cdm_cbioportal_etl/utils/get_anchor_dates.py
@@ -37,14 +37,21 @@ def get_anchor_dates():
     df_path_filt = df_path[logic_filt].copy()
     df_path_filt['DMP_ID_DERIVED'] = df_path_filt['SAMPLE_ID'].apply(lambda x: x[:9])
 
-    # Remove cases where DMP_ID does not match DMP_ID Derived
-    df_path_sample_id_error = df_path_filt[df_path_filt['DMP_ID_DERIVED'] != df_path_filt['DMP_ID']]
+    # Find DMP_IDs that do not match DMP_ID Derived
+    mrn_test1 = df_path_filt.groupby(['MRN'])['DMP_ID'].nunique()
+    list_id_prob1 = list(mrn_test1[mrn_test1 > 1].index)
+
+    mrn_test2 = df_path_filt.groupby(['DMP_ID'])['MRN'].nunique()
+    list_id_prob2 = list(mrn_test2[mrn_test2 > 1].index)
+
+    df_prob1 = df_path_filt[df_path_filt['DMP_ID_DERIVED'] != df_path_filt['DMP_ID']]
+    df_prob2 = df_path_filt[df_path_filt['DMP_ID'].isin(list_id_prob2) | df_path_filt['MRN'].isin(list_id_prob1)]
+    df_path_sample_id_error = pd.concat([df_prob1, df_prob2], axis=0).drop_duplicates()
 
     df_path_filt_clean1 = df_path_filt[df_path_filt['DMP_ID_DERIVED'] == df_path_filt['DMP_ID']]
     df_path_filt_clean1 = df_path_filt_clean1.sort_values(by=['MRN', 'DTE_TUMOR_SEQUENCING'])
 
     df_path_g = df_path_filt_clean1.groupby(['MRN', 'DMP_ID'])['DTE_TUMOR_SEQUENCING'].min().reset_index()
-
 
     # Remove any MRN or DMP-ID in df_path_g_error
     filt_rmv_patients = df_path_g['DMP_ID'].isin(df_path_sample_id_error['DMP_ID']) | \
