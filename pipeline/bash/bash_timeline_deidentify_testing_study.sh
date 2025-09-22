62be3376c165a4f796b9e46cfc95961401d264b9
fongc2 <fongc2@mskcc.org>
2025-09-22 12:44:02 -0400

    Adding bash scripts to run timeline and summary scripts autonomously

--- pipeline/bash/bash_timeline_deidentify_testing_study.sh
+++ pipeline/bash/bash_timeline_deidentify_testing_study.sh
@@ -2,13 +2,22 @@
 
 set -e
 
-CONDA_ENV_NAME="cdm-cbioportal-etl"
+CONDA_INSTALL_PATH=/gpfs/mindphidata/fongc2/miniconda3
+CONDA_ENV_NAME=conda-env-cdm-fongc2)
+YAML_CONFIG=config/etl_config_mskimpact.yml
+MINIO_ENV=/gpfs/mindphidata/fongc2/minio_env_dev.txt
+SAMPLE_LIST=/gpfs/mindphidata/cdm_repos_dev/dev/data/impact-data/mskimpact/data_clinical_sample.txt
+PATH_DATAHUB=/gpfs/mindphidata/cdm_repos_dev/dev/data/cdm-data/mskimpact
 
-YAML_CONFIG=$1
+test -n "$CONDA_INSTALL_PATH"
+test -n "$CONDA_ENV_NAME"
 test -n "$YAML_CONFIG"
+test -n "$MINIO_ENV"
+test -n "$SAMPLE_LIST"
+test -n "$PATH_DATAHUB"
 
 # Activate virtual env
-source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
+source $CONDA_INSTALL_PATH/etc/profile.d/conda.sh
 conda activate "$CONDA_ENV_NAME"
 
 MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
@@ -18,6 +27,8 @@ cd ../timeline
 # Get variables
 SCRIPT="cbioportal_timeline_deid.py"
 
-# Run script
-python $SCRIPT --config_yaml=$YAML_CONFIG
+echo $SCRIPT
+echo $YAML_CONFIG
 
+# Run script
+python $SCRIPT --config_yaml=$YAML_CONFIG --minio_env=$MINIO_ENV --cbio_sample_list=$SAMPLE_LIST --path_datahub=$PATH_DATAHUB
