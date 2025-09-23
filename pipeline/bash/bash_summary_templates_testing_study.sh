9c605de21455ea8acce2a51107159e1a2111ba5b
fongc2 <fongc2@mskcc.org>
2025-09-23 15:18:52 -0400

    updating test scripts for mskimpact assay

--- pipeline/bash/bash_summary_templates_testing_study.sh
+++ pipeline/bash/bash_summary_templates_testing_study.sh
@@ -2,20 +2,20 @@
 
 set -e
 
+EXCLUDE_LIST=$7
+
 CONDA_INSTALL_PATH=/gpfs/mindphidata/fongc2/miniconda3
 CONDA_ENV_NAME=conda-env-cdm-fongc2
 YAML_CONFIG=/gpfs/mindphidata/fongc2/github/cdm-cbioportal-etl/config/etl_config_mskimpact.yml
 MINIO_ENV=/gpfs/mindphidata/fongc2/minio_env_dev.txt
 SAMPLE_LIST=/gpfs/mindphidata/cdm_repos_dev/dev/data/impact-data/mskimpact/data_clinical_sample.txt
-PATH_DATAHUB=/gpfs/mindphidata/cdm_repos_dev/dev/data/cdm-data/mskimpact
-TEST=test
+
 
 test -n "$CONDA_INSTALL_PATH"
 test -n "$CONDA_ENV_NAME"
 test -n "$YAML_CONFIG"
 test -n "$MINIO_ENV"
 test -n "$SAMPLE_LIST"
-test -n "$PATH_DATAHUB"
 
 # Activate virtual env
 source $CONDA_INSTALL_PATH/etc/profile.d/conda.sh
@@ -23,13 +23,13 @@ conda activate "$CONDA_ENV_NAME"
 
 MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 cd $MY_PATH
-cd ../timeline
+cd ../utils
 
 # Get variables
-SCRIPT="cbioportal_timeline_deid.py"
+SCRIPT="generate_cbioportal_template.py"
 
-echo $SCRIPT
+echo "YAML CONFIG: "
 echo $YAML_CONFIG
 
 # Run script
-python $SCRIPT --config_yaml=$YAML_CONFIG --minio_env=$MINIO_ENV --cbio_sample_list=$SAMPLE_LIST --path_datahub=$PATH_DATAHUB --production_or_test=$TEST
+python $SCRIPT --config_yaml=$YAML_CONFIG --minio_env=$MINIO_ENV --cbio_sample_list=$SAMPLE_LIST --sample_exclude_list=$EXCLUDE_LIST
