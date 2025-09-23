bcbcd6426595d9a1877dbe4485d63ed9485e1a79
fongc2 <fongc2@mskcc.org>
2025-09-23 13:01:19 -0400

    updating for access samples

--- pipeline/bash/bash_summary_creator_testing_study.sh
+++ pipeline/bash/bash_summary_creator_testing_study.sh
@@ -4,9 +4,9 @@ set -e
 
 CONDA_INSTALL_PATH=/gpfs/mindphidata/fongc2/miniconda3
 CONDA_ENV_NAME=conda-env-cdm-fongc2
-YAML_CONFIG=/gpfs/mindphidata/fongc2/github/cdm-cbioportal-etl/config/etl_config_mskarcher.yml
+YAML_CONFIG=/gpfs/mindphidata/fongc2/github/cdm-cbioportal-etl/config/etl_config_mskaccess.yml
 MINIO_ENV=/gpfs/mindphidata/fongc2/minio_env_dev.txt
-PATH_DATAHUB=/gpfs/mindphidata/cdm_repos_dev/dev/data/cdm-data/mskarcher
+PATH_DATAHUB=/gpfs/mindphidata/cdm_repos_dev/dev/data/cdm-data/mskaccess
 TEST=test
 
 test -n "$CONDA_INSTALL_PATH"
