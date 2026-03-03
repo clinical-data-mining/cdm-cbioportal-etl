b4a8652d09dfe44f863a28f76bc6cb681c12bcce
fongc2 <fongc2@mskcc.org>
2026-03-03 16:04:06 -0500

    - Adding summary creation bash files for pathology annotations

---
+++ pipeline/bash/bash_summary_gleason.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO_LOCATION=$1
+CONDA_INSTALL_PATH=$2
+CONDA_ENV_NAME=$3
+DATABRICKS_ENV=$4
+
+test -n "$REPO_LOCATION"
+test -n "$CONDA_INSTALL_PATH"
+test -n "$CONDA_ENV_NAME"
+test -n "$DATABRICKS_ENV"
+
+# Activate virtual env
+source $CONDA_INSTALL_PATH/etc/profile.d/conda.sh
+conda activate "$CONDA_ENV_NAME"
+
+MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+cd $MY_PATH
+cd ../summary
+
+SCRIPT="cbio_gleason_summary.py"
+
+# Run script
+python $SCRIPT --databricks_env=$DATABRICKS_ENV
